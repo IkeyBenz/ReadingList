38a4f589fa33eabefa6988de85de0689ddf79dce
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-28 01:07:22 -0700

    [added] database connection, request body parsing middleware, and controllers

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const app = express();
 const { PORT } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
   console.log(`> Running on ${PORT}`);
+  require('./data/db');
 });
+
+module.exports = app;
