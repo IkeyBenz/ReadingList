fb56806fbe56638330a902659920cea152d83ef0
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-28 01:07:47 -0700

    [added] user controller. You can only create users for now

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,17 @@
+/**
+ * The user controller is responsible for handling all requests made to
+ * the /users endpoint. All routes in this controller are forwarded from /users,
+ * so for example router.post('/') in this file, is really handling a post request
+ * to '/users/'.
+ */
+const User = require('../data/models/user.model');
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.end();
+});
+
+module.exports = router;
+
