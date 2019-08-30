741b865d90e9d6a1289ff37b36f12d1228372c25
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-30 09:55:36 -0700

    Modified static folder to read from react's build folder

--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ const path = require('path');
 const app = express();
 const { PORT } = process.env;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware
 app.use(require('cookie-parser')());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// Backend Controllers
 app.use(require('./controllers/'));
 
+// React Client
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+
 app.listen(PORT, () => {
-  console.log(`> Running on ${PORT}`);
+  console.log(`> Running on 0.0.0.0:${PORT}`);
   require('./data/db');
 });
 
