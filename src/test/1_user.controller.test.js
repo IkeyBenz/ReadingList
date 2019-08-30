1a6f35875dd55d778ae1bf505afdb86247132406
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-29 19:27:13 -0700

    Renamed tests so they fire in a specific order

--- src/test/1_user.controller.test.js
+++ src/test/1_user.controller.test.js
@@ -4,7 +4,7 @@ chai.use(require('chai-http'));
 
 require('dotenv').config();
 
-const app = require('../');
+const app = require('..');
 const User = require('../data/models/user.model');
 const { mockUser } = require('../data/mock-data');
 
@@ -13,28 +13,26 @@ describe("User Controller Tests", () => {
 
   describe("POST /users/ with valid body", () => {
 
+    beforeEach(async () => await User.deleteMany({}));
+
+    const request = () => chai.request(app).post('/users/').send(mockUser);
+
     it("Creates new user in db", async () => {
       const originalUserCount = await User.countDocuments();
-      return chai.request(app).post('/users/').send(mockUser).then(async (err, res) => {
+      return request().then(async (res) => {
         const newUserCount = await User.countDocuments();
         expect(originalUserCount + 1).to.equal(newUserCount);
       });
     });
 
     it("Hashes the password", () => {
-      return chai.request(app).post('/users/').send(mockUser).then(async (err, res) => {
+      return request().then(async (res) => {
         const { password } = await User.findOne({}, '', { sort: { 'created_at': -1 } });
         expect(mockUser.password).to.not.equal(password);
       });
     });
 
-    it("Redirects to verify email page", () => {
-
-    });
-
   });
-
-
 
 
 });
