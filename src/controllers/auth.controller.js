eb724d41b405834291cabb0f94db5fc9bf32c127
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-30 11:19:10 -0700

    Added password matching requirement for login (obviously required)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,18 @@
 const authRouter = require('express').Router();
 const User = require('../data/models/user.model');
 
-authRouter.post('/signin', async (req, res) => {
-
+authRouter.post('/signin', async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email }, 'email password');
-    const credential = user.generateCredential();
-    res.cookie('rl-cred', credential, { maxAge: 1000 * 60 * 60 * 24 * 60 });
-    res.redirect('/');
-  } catch (e) {
-    res.status(400).send(e);
+    const passwordMatches = user && await user.comparePassword(req.body.password);
+    if (passwordMatches) {
+      const credential = user.generateCredential();
+      res.cookie('rl-cred', credential, { maxAge: 1000 * 60 * 60 * 24 * 60 });
+      return res.redirect('/');
+    }
+    return res.redirect('/signin/?error=Invalid email or password.');
+  } catch (err) {
+    next(err);
   }
 });
 
