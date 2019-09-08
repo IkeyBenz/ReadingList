d5708bd69a6a790cdff8b28c78ca3e4e4772251e
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-08 04:23:37 -0700

    [added] books controller to main controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,7 @@ const { checkAuth } = require('./middleware');
 router.use(checkAuth);
 router.use('/users', require('./user.controller'));
 router.use('/auth', require('./auth.controller'));
+router.use('/books', require('./book.controller'));
 
 router.use((error, req, res, next) => {
   if (error) {
