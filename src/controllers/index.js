eb5e4cb305a2da1e3f9a800b55e3c0dc4c87ca3e
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-08 02:34:57 -0700

    [added] error catcher to main controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const { checkAuth } = require('./middleware');
 
+router.use(checkAuth);
 router.use('/users', require('./user.controller'));
 router.use('/auth', require('./auth.controller'));
+
+router.use((error, req, res, next) => {
+  if (error) {
+    res.json(error);
+  }
+  next();
+});
 
 module.exports = router;
