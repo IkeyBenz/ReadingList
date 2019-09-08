5c6b32ffea7acff5aeea3e7b1eb4ed51689c0b9c
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-08 02:38:10 -0700

    [refactor and addition] using react hooks instead of class component. Also added logic to register the authenticated user

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
-import { Header } from './components/';
+import React, { useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import cookie from 'react-cookies';
+import { Header } from './components/';
 import { HomePage, SignupPage, SigninPage, FriendsPage } from './layouts/';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <BrowserRouter>
-          <Route exact path="/" render={() => <HomePage />} />
-          <Route exact path="/signup/" render={() => <SignupPage />} />
-          <Route exact path="/signin/" render={() => <SigninPage />} />
-          <Route exact path="/friends/" render={() => <FriendsPage />} />
-        </BrowserRouter>
-      </div>
-    )
+function App() {
+  const [state, setState] = useState({
+    user: false
+  });
+
+  if (cookie.load('rl-cred') && !state.user) {
+    fetch('/auth/current-user/').then(res => res.json()).then(({ user }) => {
+      localStorage.setItem('rl-user', JSON.stringify(user));
+      setState({ user });
+    });
   }
+
+  return (
+    <div>
+      <Header user={state.user} />
+      <BrowserRouter>
+        <Route exact path="/" render={() => <HomePage />} />
+        <Route exact path="/signup/" render={() => <SignupPage />} />
+        <Route exact path="/signin/" render={() => <SigninPage />} />
+        <Route exact path="/friends/" render={() => <FriendsPage />} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
