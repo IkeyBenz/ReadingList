2c392622347115f748a3cd1b2bc37819ee14b5f2
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-08 04:24:07 -0700

    [refactor and additions] converted class component to functional using react hooks. Also added content for authenticated users.

--- src/client/src/layouts/HomePage.js
+++ src/client/src/layouts/HomePage.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HomePage extends Component {
-
-  render() {
-    return (
-      <div className="container-fluid">
+function HomePage({ user }) {
+  return (
+    <div className="container-fluid">
+      {!user &&
         <center className="container mt-5">
           <h1>What are you reading?</h1>
           <h4 className="text-muted">Reading list allows you to keep track of books you've read, and intend to read, while also sharing that information with your friends!</h4>
@@ -13,10 +12,31 @@ class HomePage extends Component {
             <a href="/signup/" className="btn btn-secondary ml-1">Sign Up</a>
           </div>
         </center>
+      }
+      {user &&
+        <div className="card">
+          <div className="card-header">
+            <h3 className="card-title">{user.name}'s Reading List:</h3>
+          </div>
+          <div className="card-body">
+            <ul className="list-group">
+              {user.books.map(book => (
+                <li className="list-group-item">{book}</li>
+              ))}
+            </ul>
+          </div>
+          <form className="card-footer form-inline" enctype="application/json" action="/books/" method="POST">
+            <input className="form-control" type="text" name="book[author]" placeholder="Book Author" required />
+            <input className="form-control" type="text" name="book[title]" placeholder="Book Title" required />
+            <button className="btn btn-success">Add Book</button>
+          </form>
+        </div>
 
-      </div>
-    );
-  }
+      }
+
+
+    </div>
+  );
 }
 
 export default HomePage;
